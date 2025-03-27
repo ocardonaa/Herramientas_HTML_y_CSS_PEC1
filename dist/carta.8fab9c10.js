d9ae049fff9a058f12a5c8fdfa7b0046de001906
Oriol Cardona Alsina <ocardonaa@uoc.edu>
2025-03-27 13:38:08 +0100

    attempt 1

--- dist/carta.8fab9c10.js
+++
@@ -1,2 +0,0 @@
-document.querySelectorAll(".container-card").forEach(e=>{let c=e.querySelector(".front-card"),l=e.querySelector(".back-card");c.addEventListener("click",function(){c.style.display="none",l.style.display="block"}),l.addEventListener("click",function(){c.style.display="block",l.style.display="none"})});
-//# sourceMappingURL=carta.8fab9c10.js.map
