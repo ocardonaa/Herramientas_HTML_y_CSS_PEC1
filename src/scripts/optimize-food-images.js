1031993e1e4b71cc0a04510ed57f79edee08ca06
Oriol Cardona Alsina <ocardonaa@uoc.edu>
2025-04-12 18:34:29 +0200

    added responsiveness to images on carta.html

--- src/scripts/optimize-food-images.js
+++ src/scripts/optimize-food-images.js
@@ -6,4 +6,6 @@ const outputDir = path.join(__dirname, '../img/optimized-dishes');
 
 const images = ['food1.jpg', 'food2.jpg', 'food3.jpg', 'food4.jpg', 'food5.jpg', 'food6.jpg'];
 
-optimizeScript.optimizeImages(inputDir, outputDir, images, 500, 500);
+optimizeScript.optimizeImages(inputDir, outputDir, images, 300, 300, '-phone');
+optimizeScript.optimizeImages(inputDir, outputDir, images, 300, 300, '-tablet');
+optimizeScript.optimizeImages(inputDir, outputDir, images, 400, 400, '-desktop');
