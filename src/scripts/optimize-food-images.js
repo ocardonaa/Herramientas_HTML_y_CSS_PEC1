819b59b7b952de28d6d92777c8834bd1d681b2af
Oriol Cardona Alsina <ocardonaa@uoc.edu>
2025-04-13 19:33:43 +0200

    finished responsiveness

--- src/scripts/optimize-food-images.js
+++ src/scripts/optimize-food-images.js
@@ -7,5 +7,5 @@ const outputDir = path.join(__dirname, '../img/optimized-dishes');
 const images = ['food1.jpg', 'food2.jpg', 'food3.jpg', 'food4.jpg', 'food5.jpg', 'food6.jpg'];
 
 optimizeScript.optimizeImages(inputDir, outputDir, images, 300, 300, '-phone');
-optimizeScript.optimizeImages(inputDir, outputDir, images, 300, 300, '-tablet');
+optimizeScript.optimizeImages(inputDir, outputDir, images, 350, 350, '-tablet');
 optimizeScript.optimizeImages(inputDir, outputDir, images, 400, 400, '-desktop');
--- src/scripts/optimize-history-images.js
+++ src/scripts/optimize-history-images.js
@@ -4,7 +4,7 @@ const optimizeScript = require('./optimize-script');
 const inputDir = path.join(__dirname, '../img/places');
 const outputDir = path.join(__dirname, '../img/optimized-places');
 
-const images = ['napoles1.jpg', 'napoles2.jpg', 'napoles3.jpg'];
+const images = [ 'luca.jpg', 'napoles1.jpg', 'napoles2.jpg', 'napoles3.jpg'];
 
 optimizeScript.optimizeImages(inputDir, outputDir, images, 300, 450, '-phone');
 optimizeScript.optimizeImages(inputDir, outputDir, images, 600, 900, '-tablet');
