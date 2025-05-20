d2357682d916cdda4f0ad38c064f7e969e6ccd80
Oriol Cardona Alsina <ocardonaa@uoc.edu>
2025-05-20 16:57:17 +0200

    changes done, v1 of the first changes

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,10 @@
 console.log('Web started');
+
+const hasNativeLazyLoadSupport = 'loading' in HTMLImageElement.prototype
+if (hasNativeLazyLoadSupport) {
+    console.log('I have lazy loading');
+}
+
+else {
+    console.log('I do not have lazy loading');
+}
