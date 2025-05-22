5673934b5d24da543e7894835c7661c7c8b29bd2
Oriol Cardona Alsina <ocardonaa@uoc.edu>
2025-05-23 01:37:04 +0200

    index improvements 68

--- src/scripts/load-luca.js
+++ src/scripts/load-luca.js
@@ -2,7 +2,7 @@ function applyConditionalLazyLoading() {
     const isPhone = window.matchMedia("(max-width: 799px)").matches;
     const firstImage = document.getElementById('luca');
     if (isPhone) {
-        firstImage.setAttribute("loading", "lazy");
+        firstImage.setAttribute("loading", "eager");
     }
     else {
         firstImage.setAttribute("fetchpriority", "high");
